b9a2928608481721353025cc58b42fdd7a2eaa75
ys-zhang <ys-zhang@live.com>
2025-08-11 11:22:19 +0800

    modify github action

--- app/App/WebServer.hs
+++ app/App/WebServer.hs
@@ -1,23 +1,48 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLists #-}
 
 module App.WebServer (
   Options, optionsParser,
-  app
+  app,
+  -- * static pages 
+  staticPages, 
+  getStaticPage, 
+  getStaticPage',
 ) where
 
+import Control.Monad (void)
+import Data.Maybe (fromJust)
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.String (fromString)
+import Data.SOP.Table (renderHtmlTable)
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Lucid
+import Options.Applicative
 import Servant
 import Servant.API.ContentTypes.Lucid (HTML)
 import UHOI (conceptsMetaTable)
-import Data.SOP.Table (renderHtmlTable)
-import Options.Applicative
+import GHC.Stack (HasCallStack)
 
 data Options = Options
+  { port :: Int
+  , host :: String
+  }
 
 optionsParser :: Parser Options
-optionsParser = pure Options
+optionsParser = Options
+  <$> option auto
+      ( long "port"
+     <> short 'p'
+     <> metavar "PORT"
+     <> value 8080
+     <> help "Port number for the web server" )
+  <*> strOption
+      ( long "host"
+     <> metavar "HOST"
+     <> value "localhost"
+     <> help "Host address for the web server" )
 
 type Api = Get '[HTML] (Html ())
       :<|> "test" :> Get '[HTML] (Html ())
@@ -26,9 +51,12 @@ api :: Proxy Api
 api = Proxy
 
 app :: Options -> IO ()
-app _ = do 
-  putStrLn "Starting web server on port 8080..."
-  run 8080 . logRequests $ serve api handler
+app opts = do 
+  putStrLn $ "Starting web server on " ++ opts.host ++ ":" ++ show opts.port ++ "..."
+  let settings = setHost (fromString opts.host) 
+               $ setPort opts.port 
+               $ defaultSettings
+  runSettings settings . logRequests $ serve api handler
 
 logRequests :: Middleware
 logRequests waiApp req resp = do
@@ -37,15 +65,33 @@ logRequests waiApp req resp = do
   waiApp req resp
 
 handler :: Server Api
-handler = pure meta_table :<|> pure hello_world
+handler = pure (getStaticPage' "meta-table") :<|> pure hello_world
  where 
   hello_world = html_ $ do 
     h1_ "Hello, World!"
     p_ $ do 
       "Jump to the " <> a_ [href_ "/meta"] "meta table"
-  meta_table = html_ $ do
-    head_ $ title_ "Concepts Meta Table"
-    body_ conceptsHtmlTable
+
+staticPages :: Map FilePath (Html ())
+staticPages = 
+  [ ( "index",  getStaticPage' "meta-table")
+  , ( "meta-table"
+    , html_ $ do   
+        void . head_ $ title_ "Concepts Meta Table"
+        body_ conceptsHtmlTable
+    )
+  ]
+
+getStaticPage' :: HasCallStack => FilePath -> Html ()
+getStaticPage' = fromJust . getStaticPage
+
+getStaticPage :: FilePath -> Maybe (Html ())
+getStaticPage = flip Map.lookup staticPages
 
 conceptsHtmlTable :: Html ()
 conceptsHtmlTable = renderHtmlTable [] conceptsMetaTable
+
+-- >>> Map.keys staticPages
+-- ["index","meta-table"]
+
+
--- app/Main.hs
+++ app/Main.hs
@@ -2,38 +2,64 @@
 
 module Main where
 
+import App.WebServer qualified as WS
+import Control.Monad (when, forM_)
 import Data.SOP.Table
+import Data.Map.Strict qualified as Map
 import Data.Text.Lazy qualified as TL
 import Lucid
-import UHOI (conceptsMetaTable)
 import Options.Applicative
-import App.WebServer qualified as WS
+import System.Directory
+import System.FilePath ( (</>), (<.>) )
+import UHOI (conceptsMetaTable)
+
 
 data Options 
   = WebServerOptions WS.Options
   | CmdOptions CmdOptions
 
-data CmdOptions = GenHtml | PrettyPrint
+data CmdOptions 
+  = PrettyPrint
+  | GenHtml GenHtmlOptions
+
+newtype GenHtmlOptions = GenHtmlOptions
+  { outputPath :: Maybe FilePath
+  }
 
 optionsParser :: Parser Options
 optionsParser = WebServerOptions <$> parse_web_options
               <|> CmdOptions <$> parse_cmd_options
  where
-  parse_web_options = subparser
-    ( command "run-server" (info WS.optionsParser (progDesc "Run the web server"))
+  parse_web_options = hsubparser
+    ( command "run-server" 
+        (info WS.optionsParser  
+              (progDesc "Run the web server (options: --port, --host)")
+        )
     )
-  parse_cmd_options = subparser
-    ( command "gen-html" (info (pure GenHtml) (progDesc "Generate HTML"))
-    <> command "pretty-print" (info (pure PrettyPrint) (progDesc "Pretty print"))
+  parse_cmd_options = hsubparser
+    (  command "gen-html" 
+         (info (GenHtml <$> parseGenHtmlOptions) 
+               (progDesc "Generate HTML (options: -o/--output)")
+         )
+    <> command "pretty-print" 
+         (info (pure PrettyPrint <**> helper) 
+               (progDesc "Pretty print the concept meta table")
+         )
     )
+  parseGenHtmlOptions = GenHtmlOptions
+    <$> optional (strOption
+        ( long "output"
+       <> short 'o'
+       <> metavar "FILE"
+       <> help "Output file for generated HTML" ))
 
 
 main :: IO ()
 main = do
-  opts <- execParser $ info (optionsParser <**> helper)
-    ( fullDesc
-   <> progDesc "Run the application"
-   <> header "uhoi - a Haskell application" )
+  opts <- customExecParser (prefs showHelpOnEmpty) (info (optionsParser <**> helper)
+             ( fullDesc
+            <> progDesc "UHOI command line interface"
+            <> header "uhoi - a Haskell application" ))
   case opts of
     WebServerOptions wsOpts -> WS.app wsOpts
     CmdOptions cmdOpts -> runCmd cmdOpts
@@ -41,17 +67,24 @@ main = do
   runCmd PrettyPrint = do
     putStrLn "\nConcept Meta Table:"
     putStrLn $ prettyHTable conceptsMetaTable
-  runCmd GenHtml = do
-    putStrLn . prettyPrintHtml $ do 
-      html_ $ do
-        head_ $ title_ "Concepts Meta Table"
-        body_ conceptsHtmlTable
-
-conceptsHtmlTable :: Html ()
-conceptsHtmlTable = renderHtmlTable [] conceptsMetaTable
-
--- mtable :: UI.MetaTable '[ConceptExample]
--- mtable = UI.table (Proxy @'[ConceptExample]) (hpure Proxy)
+  runCmd (GenHtml opts) = do
+    putStrLn "Generating HTML...\n"
+    case opts.outputPath of 
+      Nothing -> do 
+        putStrLn 
+          $ prettyPrintHtml 
+          $ WS.getStaticPage' "meta-table"
+      Just path -> do
+        putStrLn $ "Writing HTML to " ++ path
+        output_exists <- doesDirectoryExist path
+        when output_exists do
+          putStrLn $ "Warning: Output directory already exists, remove the path: " 
+                   ++ path
+          removeDirectoryRecursive path
+        createDirectoryIfMissing True path
+        forM_ (Map.toList WS.staticPages) \(route, html) -> do
+          writeFile (path </> route <.> "html") 
+                    (prettyPrintHtml html)
 
 prettyPrintHtml :: Html () -> String
 prettyPrintHtml html = raw_str
